db5c4708e49767b055d597eb40aaddb943ac9b6e
iabbes <abbes_ines@hotmail.fr>
2023-01-12 12:05:00 +0100

    wip

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -1,5 +1,11 @@
 <?php
-
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Tuupola\Middleware\HttpBasicAuthentication;
+use \Firebase\JWT\JWT;
+require DIR . '/../vendor/autoload.php';
+require_once DIR . '/../bootstrap.php';
 require_once __DIR__ . '/models/client.php';
 require_once __DIR__ . '/models/avion.php';
 
